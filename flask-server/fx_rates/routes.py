690af038c85260800fb2c73e6cd0086e365ee1bc
Nick Dilallo <nickd5465@gmail.com>
2024-10-17 11:57:09 -0500

    basic pair conversion puller

---
+++ flask-server/fx_rates/routes.py
@@ -0,0 +1,45 @@
+import requests
+from flask import jsonify
+from . import fx_rates_bp
+
+EXCHANGE_API_KEY = "b2d7bed5405d85e5d85ce1b0"
+TOP_10_CURRENCIES = ["KWD", "BHD", "OMR", "JOD", "GBP", "GIP", "KYD", "EUR", "CHF", "USD"]
+
+@fx_rates_bp.route('/api/fx-rates', methods=['GET'])
+def get_fx_rates():
+    results = []  # List to hold conversion data
+
+    for i in range(len(TOP_10_CURRENCIES)):
+        for j in range(i, len(TOP_10_CURRENCIES)):
+            if i != j:
+                conversion_data = get_fx_rate_pairs(TOP_10_CURRENCIES[i], TOP_10_CURRENCIES[j])
+                print(conversion_data)
+                if conversion_data:
+                    results.append(conversion_data)
+
+    return jsonify({
+        "result": "success",
+        "data": results  # Return all conversion rates in the response
+    })
+
+
+def get_fx_rate_pairs(curr1, curr2):
+    url = f'https://v6.exchangerate-api.com/v6/{EXCHANGE_API_KEY}/pair/{curr1}/{curr2}'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        data = response.json()
+
+        conversion_rate = data.get("conversion_rate")
+
+        if conversion_rate is not None:
+            return {
+                "from": curr1,
+                "to": curr2,
+                "conversion_rate": conversion_rate
+            }
+        else:
+            return None  # Return None if conversion rate is not found
+    else:
+        print(f'Failed to fetch data for {curr1} to {curr2}: {response.status_code}')
+        return None  # Return None if there was an error in fetching the data
--- flask-server/main.py
+++ flask-server/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
+from fx_rates import fx_rates_bp
 
 
 app = Flask(__name__)
@@ -8,6 +9,8 @@ CORS(app)
 # @app.route('/hello', methods=['GET'])
 # def hello_world():
 #     return jsonify({"message": "Frit"})
+
+app.register_blueprint(fx_rates_bp)
 
 
 if __name__ == '__main__':
