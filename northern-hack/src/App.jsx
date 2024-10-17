badab955898b25e1a97e927a6eff2727df244f27
jjdubski <101855602+jjdubski@users.noreply.github.com>
2024-10-17 18:23:12 -0500

    more pages and routes

--- northern-hack/src/App.jsx
+++ northern-hack/src/App.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Index from './pages/index';
 import Trade from './pages/trade/trade';
+import Portfolio from './pages/portfolio/portfolio';
+import Rates from './pages/rates/rates';
 import "./App.css"
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/trade" element={<Trade />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/rates" element={<Rates />} />
         </Routes>
     </Router>
   );
