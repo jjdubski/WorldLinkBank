128099255ad503b25484b5a98de98c71420c0db1
serhat111 <49848798+serhat111@users.noreply.github.com>
2024-10-17 11:38:50 -0500

    added navbar changes

--- northern-hack/src/components/navbar/Navbar.jsx
+++ northern-hack/src/components/navbar/Navbar.jsx
@@ -4,7 +4,17 @@ import "./navbar.css"
 const Navbar = () => {
   return (
     <div className='navbar-cont'>
-        
+        <div className="navbar-left">
+
+        </div>
+        <div className="navbar-right">
+            <div className="menu-cont">
+                <p className="menu-item">Transactions</p>
+                <p className="menu-item">Account</p>
+                <p className="menu-item">Rates</p>
+                <button className="menu-item-button">Dashboard</button>
+            </div>
+        </div>
     </div>
   )
 }
