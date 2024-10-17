2a645c1f2f16a7fd429337ebd52af7a7e2ecbfc4
jjdubski <101855602+jjdubski@users.noreply.github.com>
2024-10-17 18:06:49 -0500

    added pages and links

--- northern-hack/src/components/welcome/Welcome.jsx
+++ northern-hack/src/components/welcome/Welcome.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import "./welcome.css"
+import { Link } from 'react-router-dom';
 
 const Welcome = () => {
   return (
     <div className='welcome-cont'>
         <div className='text-align-welcome'>
-        <h1>Welcome to World Link Bank</h1>
-        <h3>At WorldLink, we specialize in providing comprehensive FX trading solutions that empower investors to navigate the complexities of the global currency markets. Our expert team combines cutting-edge technology, deep market insights, and a client-focused approach to deliver consistent results for traders of all levels.</h3>
-        <button className="welcome-button">Dashboard</button>
+          <h1>Welcome to World Link Bank</h1>
+          <h3>At WorldLink, we specialize in providing comprehensive FX trading solutions that empower 
+            investors to navigate the complexities of the global currency markets. Our expert team 
+            combines cutting-edge technology, deep market insights, and a client-focused approach to deliver consistent results for traders of all levels.</h3>
+          <Link to="/trade"><button className="welcome-button">Start Trading FX</button></Link>
+      
         </div>
     </div>
   )
---
+++ northern-hack/src/pages/trade/trade.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./trade.css";
+import Navbar from "../../components/navbar/Navbar";
+
+const Trade = () => {
+    return (
+        <>
+        <Navbar/>
+        <div className='trade'>
+        </div>
+        </>
+    )
+}
+
+export default Trade;
