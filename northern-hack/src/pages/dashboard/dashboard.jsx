e9ed229e9ab2ccfb3fc0171b928df2028818a2e0
serhat111 <49848798+serhat111@users.noreply.github.com>
2024-10-18 08:14:14 -0500

    added changes to dashboard

--- northern-hack/src/pages/dashboard/dashboard.jsx
+++ northern-hack/src/pages/dashboard/dashboard.jsx
@@ -2,11 +2,13 @@ import React, { useEffect, useState } from "react";
 import "./dashboard.css";
 import Navbar from "../../components/navbar/Navbar";
 import axios from 'axios';
+import Prediction from "../../components/prediction/Prediction";
 
 const Dashboard = () => {
     return (
         <>
         <Navbar/>
+        <Prediction/>
         </>
     )
 }
