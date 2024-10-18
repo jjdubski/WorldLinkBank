2bd819439b30b3f7ac3f0e6c4cb19930ef4f31a2
Nick Dilallo <nickd5465@gmail.com>
2024-10-18 04:38:19 -0500

    Interpolated (spline) future vals for predictors

--- flask-server/currency_ml_model/future_predictions/future_data_preprocessing.py
+++ flask-server/currency_ml_model/future_predictions/future_data_preprocessing.py
@@ -30,6 +30,7 @@ def merge(df):
     merged_df = pd.merge(df, full_df, left_on='Date', right_on='Month_Year', how='outer')
 
     merged_df['Combined_Date'] = merged_df['Date'].combine_first(merged_df['Month_Year'])
+    merged_df['Combined_Date'] = merged_df['Combined_Date'].apply(lambda x: f"{x}-01" if len(x.split('-')) == 2 else x)
 
     merged_df.to_csv('./currency_ml_model/datasets/prediction_sets/merged_data.csv', index=False)
 
@@ -37,9 +38,11 @@ def merge(df):
 
 def predict_future_vals(df):
     df.set_index('Combined_Date', inplace=True)
+    df.index = pd.to_datetime(df.index, format='%Y-%m-%d')
 
     print(df.head())
 
-    df['Rate_y'] = df['Rate_y'].interpolate(method='linear')
-    df['CPI_y'] = df['CPI_y'].interpolate(method='linear')
+    # df['Rate_y'] = df['Rate_y'].interpolate(method='linear')
+    df['Rate_y'] = df['Rate_y'].interpolate(method='spline', order=1)
+    df['CPI_y'] = df['CPI_y'].interpolate(method='spline', order=2)
     df.to_csv('./currency_ml_model/datasets/prediction_sets/post_interpolation_data.csv', index=False)
