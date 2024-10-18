bfb22430ce81d7c5d555561b7d62def9345d7632
Nick Dilallo <nickd5465@gmail.com>
2024-10-18 11:03:39 -0500

    adding back

--- flask-server/currency_ml_model/future_predictions/send_predictions.py
+++ flask-server/currency_ml_model/future_predictions/send_predictions.py
@@ -3,12 +3,12 @@ from flask import Blueprint
 import pandas as pd
 from flask_cors import CORS
 
-PREDICTION_DATASET = "./currency_ml_model/datasets/prediction_sets/new_final_full.csv"
+PREDICTION_DATASET = "./currency_ml_model/datasets/prediction_sets/future_prediction_data.csv"
 
 currency_ml_model_bp = Blueprint('currency_ml_model', __name__)
 CORS(currency_ml_model_bp)
 
-# @currency_ml_model_bp.route('/api/future-predictions', methods=['GET'])
+@currency_ml_model_bp.route('/api/future-predictions', methods=['GET'])
 def get_future_predictions():
     data = get_prediction_values()
     return data
@@ -16,6 +16,5 @@ def get_future_predictions():
 def get_prediction_values():
     df = pd.read_csv(PREDICTION_DATASET)
     json_data = df.to_json(orient='records')
-    print(json_data)
     
     return json_data
