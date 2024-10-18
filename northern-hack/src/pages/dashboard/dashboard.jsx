dcb11dcffa7c2772652cbc9c6bd337f6c29d4ee7
jjdubski <101855602+jjdubski@users.noreply.github.com>
2024-10-17 19:14:28 -0500

    fixed chart and added code to trade page

---
+++ northern-hack/src/pages/dashboard/dashboard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./dashboard.css";
+import Navbar from "../../components/navbar/Navbar";
+
+const Dashboard = () => {
+    return (
+        <>
+        <Navbar/>
+        <div className='dashboard'>
+        </div>
+        </>
+    )
+}
+
+export default Dashboard;
--- northern-hack/src/pages/trade/trade.jsx
+++ northern-hack/src/pages/trade/trade.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import "./trade.css";
 import Navbar from "../../components/navbar/Navbar";
+import TradeUI from "../../components/tradeUI/TradeUI";
 
 const Trade = () => {
     return (
         <>
         <Navbar/>
         <div className='trade'>
+            <TradeUI/>
         </div>
         </>
     )
