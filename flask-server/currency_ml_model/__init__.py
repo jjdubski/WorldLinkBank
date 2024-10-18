df60e716b9b10a12cba45c8f19af5c35f3abd7b8
Nick Dilallo <nickd5465@gmail.com>
2024-10-18 10:46:13 -0500

    model improve

--- flask-server/currency_ml_model/__init__.py
+++ flask-server/currency_ml_model/__init__.py
@@ -4,11 +4,11 @@ from . import model
 from . import evaluate
 from . import data_processing
 from .future_predictions.future_data_preprocessing import run
-from .future_predictions.conversion_prediction_chart import create_output_chart, predict_on_dataset, fix_original_file,predict_data
+from .future_predictions.conversion_prediction_chart import create_output_chart
 
 currency_ml_model_bp = Blueprint('currency_ml_model', __name__)
 
-from .future_predictions.send_predictions import get_future_predictions
+# from .future_predictions.send_predictions import get_future_predictions
 # from flask import Blueprint
 
 # data_preprocessing.load_data()
@@ -28,12 +28,7 @@ from .future_predictions.send_predictions import get_future_predictions
 # model.train_model()
 
 # run()
-# model = create_output_chart()
-# predict_on_dataset(model)
-# fix_original_file()
+# create_output_chart()
 
 # future_predictions_bp = Blueprint('future_predictions', __name__)
 # get_future_predictions()
-
-# predict_data()
-get_future_predictions()
